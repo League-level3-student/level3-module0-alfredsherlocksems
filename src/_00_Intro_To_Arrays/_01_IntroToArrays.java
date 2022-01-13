03827c1b089ac74eb7d5a0b6cd9d8df6af744651
student <student@ip-172-31-54-76.us-west-2.compute.internal>
2022-01-12 16:57:38 -0800

    arrays

--- src/_00_Intro_To_Arrays/_01_IntroToArrays.java
+++ src/_00_Intro_To_Arrays/_01_IntroToArrays.java
@@ -5,32 +5,58 @@ import java.util.Random;
 public class _01_IntroToArrays {
     public static void main(String[] args) {
         // 1. declare and Initialize an array 5 Strings
-
+    	String[] names = new String[5];
+    	names[0] = "Bob";
+    	names[1] = "Andy";
+    	names[2] = "Rob";
+    	names[3] = "Sam";
+    	names[4] = "Sally";
         // 2. print the third element in the array
-
+    	System.out.println(names[2]);
         // 3. set the third element to a different value
-
+    	names[2] = "Kate";
         // 4. print the third element again
-
+    	System.out.println(names[2]);
         // 5. use a for loop to set all the elements in the array to a string
         //    of your choice
-
+    	for (int i = 0; i < names.length; i++) {
+    		names[i] = "James";
+    	}
         // 6. use a for loop to print all the values in the array
         //    BE SURE TO USE THE ARRAY'S length VARIABLE
-
+    	for (int i = 0; i < names.length; i++) {
+    		System.out.println(names[i]);
+    	}
         // 7. make an array of 50 integers
-
+    	int[] numList = new int[50];
         // 8. use a for loop to make every value of the integer array a random
         //    number
-
+    	Random ran = new Random();
+    	for (int i = 0; i < numList.length; i++) {
+    		numList[i] = ran.nextInt(101);
+    	}
         // 9. without printing the entire array, print only the smallest number
         //    on the array
-
+    	int smallestNum = numList[0];
+    	for (Integer i: numList) {
+    		if (smallestNum > i) {
+    			smallestNum = i;
+    		}
+    	}
+    	System.out.println("smallestNumber = " + smallestNum);
         // 10 print the entire array to see if step 8 was correct
-
+    	for  (Integer i: numList) {
+    		System.out.println(i);
+    	}
         // 11. print the largest number in the array.
-
+    	int largestNumber = numList[0];
+    	for (Integer i: numList) {
+    		if (largestNumber < i) {
+    			largestNumber = i;
+    		}
+    	}
+    	System.out.println("largestNumber = " + largestNumber);
         // 12. print only the last element in the array
-
+    	System.out.println(numList[49]);
     }
 }
--- src/_01_Simple_Array_Algorithms/MoreArrayFun.java
+++ src/_01_Simple_Array_Algorithms/MoreArrayFun.java
@@ -4,23 +4,68 @@ import java.util.Random;
 
 public class MoreArrayFun {
     //1. Create a main method to test the other methods you write.
-
-
+	public static void main (String [] args) {
+		String[] array = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"};
+		MoreArrayFun a = new MoreArrayFun();
+		a.orderedArrays(array);
+		System.out.println("Next");
+		a.reverseOrderedArrays(array);
+		System.out.println("Next");
+		a.moduloArrays(array);
+		System.out.println("Next");
+		a.ranArrays(array);
+		}
 
     //2. Write a method that takes an array of Strings and prints all the Strings in the array.
 
-
+	void orderedArrays(String[] array) {
+		for (String i: array) {
+			System.out.println(i);
+		}
+	}
 
     //3. Write a method that takes an array of Strings and prints all the Strings in the array
     //   in reverse order.
-
+	void reverseOrderedArrays(String[] array) {
+		for (int i = array.length - 1; i >= 0; i--) {
+			System.out.println(array[i]);
+		}
+	}
 
 
     //4. Write a method that takes an array of Strings and prints every other String in the array.
-
+	void moduloArrays(String[] array) {
+		for (int i = 0; i < array.length; i++) {
+			if (i % 2 == 0) {
+				System.out.println(array[i]);
+			}
+		}
+	}
 
     //5. Write a method that takes an array of Strings and prints all the Strings in the array
     //   in a completely random order. Almost every run of the program should result in a different order.
-
+	void ranArrays(String[] array) {
+		Random ran = new Random();
+		int[] indexes = new int[array.length];
+		boolean moreToGo = true;
+		while(moreToGo) {
+			int i = ran.nextInt(array.length);
+			if (indexes[i] != -1) {
+				System.out.println(array[i]);
+				indexes[i] = -1;
+				int count = 0;
+				for (int j: indexes) {
+					count+=1;
+					if (j != -1) {
+						break;
+					}
+					if (count == indexes.length) {
+						moreToGo = false;
+					}
+				}
+			}
+			
+		}
+	}
 
 }
